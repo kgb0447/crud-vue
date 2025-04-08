741e039ac699b5b4ea87423a87b64f7c8a2f808a
kael0447 <kaelgarciabennick1997@gmail.com>
2025-04-08 17:19:03 +0800

    feat: added dynamic field and configuration per field

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,100 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { computed, onUpdated, ref} from 'vue'
+
+const inputs = ref({});
+const tasks = ref([]);
+const handleSubmit = (e) => {
+  e.preventDefault();
+  tasks.value.push({ ...inputs.value }) 
+  inputs.value = {initialState}
+}
+
+const fields = [
+  {id: 1, label:'Title', value: 'title',type: 'text'},
+  {id: 2, label:'Description', value: 'description',type: 'textarea'},
+  {id: 3, label:'Start Date: ', value: 'date',type: 'date'},
+]
+
+console.log("Data :",inputs.value)
 </script>
 
 <template>
-  <header>
-
+  <div class="container">
     <div class="wrapper">
-    </div>
-  </header>
+      <form @submit="handleSubmit" method="post">
+        <div v-for="field in fields">
+          <label :for="field.value">{{field.label}}</label>
+          <input v-if="field.type !== 'textarea'" :type="field.type" :name="field.value" :id="field.value" class="fields" :placeholder="`Please enter your ${field.label}: `" v-model="inputs[field.value]" autocomplete="off">
+          <textarea v-else-if="field.type === 'textarea'" :name="field.value" :id="field.value" cols="30" rows="10" class="fields textarea" :placeholder="`Please enter your ${field.label}: `"  v-model="inputs[field.value]"></textarea>
+      </div>
 
-  <main>
-  </main>
+      {{ console.log(Object.values(inputs).some(item => item?.length >= 0)) }}
+      <div class="btnWrapper">
+      <input type="submit" value="Submit" class="btn">
+    </div>
+      </form>
+    </div>
+  </div>
+  <div v-for="item in tasks">
+    <div>{{ item.title }}</div>
+    <div>{{ item.desc }}</div>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
 .logo {
   display: block;
   margin: 0 auto 2rem;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.container{
+  display: flex;
+  width: 100dvw;
+  height: 100dvh;
+  padding: 20px;
+  overflow-y: scroll;
 }
+  .wrapper {
+    display: flex;
+    justify-content: center;
+    background-color:red;
+    padding: 20px;
+    border-radius: 20px
+  }
+  .fields{
+    width: 100%;
+    height: 50px;
+    border-radius: 10px;
+    border: 0px;
+    padding: 10px;
+  }
+  .textarea{
+    outline: 0px;
+    border-width: 0;
+    border-radius: 10px;
+    padding: 10px;
+    resize: none;
+    height: auto;
+  }
+  .btnWrapper{
+    width: 100%;
+    display: flex;
+    justify-content: center;
+  }
+  .btn{
+    width: 80%;
+    height: 48px;
+    border-radius: 10px;
+    background-color: #ffff;
+    color: #000;
+    border-width: 0px;
+    text-align: center;
+    margin-top: 20px;
+    cursor: pointer;
+  }
+  .btn:disabled{
+    opacity: 0.5;
+    cursor:auto;
+  }
 </style>
